9264ab23af52065078d5f144e02ea66d16a7a665
funnygooberman <coffey.taylor70@gmail.com>
2022-08-19 10:08:21 -0600

    added office_sign page

---
+++ admin/config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define("_DB_SERVERNAME", "localhost");
+define("_DB_NAME", "iotcapst_backend");
+define("_DB_USERNAME", "iotcapst_user");
+define("_DB_PASSWORD", "bqX~qow-hJB#");
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
