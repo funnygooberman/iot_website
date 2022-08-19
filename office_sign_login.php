4cfa59de96d25a8f2bd1d0df6ba6f3a4240aa205
funnygooberman <coffey.taylor70@gmail.com>
2022-08-19 10:48:50 -0600

    styling

--- office_sign_login.php
+++ office_sign_login.php
@@ -46,7 +46,7 @@
                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
             </div>
             <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
+                <input type="submit" class="button" value="Login">
             </div>
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
         </form>
