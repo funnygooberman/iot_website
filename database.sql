505c54f550baf80e9d1bc359b81fc047274a1ce4
funnygooberman <coffey.taylor70@gmail.com>
2022-08-31 10:25:16 -0600

    Lets add an admin

--- database.sql
+++ database.sql
@@ -1,25 +1,26 @@
-CREATE TABLE users {
-    id INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    name VARCHAR(255),
-    image_path VARCHAR(255),
-    location VARCHAR(255),
-    message VARCHAR(10000),
-    pi_id VARCHAR(10000),
-    PRIMARY KEY (id),
-};
+CREATE TABLE users (
+    id int NOT NULL AUTO_INCREMENT,
+    username varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    name varchar(255),
+    image_path varchar(255),
+    location varchar(255),
+    message varchar(1000),
+    pi_id varchar(1000),
+    PRIMARY KEY (id)
+);
 
-CREATE TABLE displays {
-    pi_id VARCHAR(10000) NOT NULL,
+CREATE TABLE displays (
+    pi_id VARCHAR(100) NOT NULL,
     user_id INT,
     PRIMARY KEY (pi_id)
-};
+);
 
-CREATE TABLE admin {
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE admin (
+    admin_id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-};
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (admin_id)
+);
 
 
