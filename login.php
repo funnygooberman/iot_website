f43d4722004cc9c50d37c7c4e073037e31d2f15f
funnygooberman <coffey.taylor70@gmail.com>
2022-09-20 17:04:10 -0600

    Update login.php

--- login.php
+++ login.php
@@ -64,7 +64,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: officesign_dashboard.php");
+                            header("location: dashboard.php");
                         } else{
                             // Password is not valid, display a generic error message
                             $login_err = "Invalid username or password.";
