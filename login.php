af621701d4b52bfa9a3f5e7c752cdfe2fdfaf046
funnygooberman <coffey.taylor70@gmail.com>
2022-09-20 15:11:09 -0600

    Create login.php

---
+++ login.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html><!--  This site was created in Webflow. https://www.webflow.com  -->
+<!--  Last Published: Tue Sep 20 2022 21:02:13 GMT+0000 (Coordinated Universal Time)  -->
+<html data-wf-page="632a2694f17249dced877a2c" data-wf-site="632a181108141a036b8932b7">
+<head>
+  <meta charset="utf-8">
+  <title>Login</title>
+  <meta content="Login" property="og:title">
+  <meta content="Login" property="twitter:title">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta content="Webflow" name="generator">
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="css/taylors-awesome-site-ed8a4b.webflow.css" rel="stylesheet" type="text/css">
+  <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+  <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+  <link href="images/webclip.png" rel="apple-touch-icon">
+</head>
+<body class="body-2">
+  <div data-animation="default" data-collapse="medium" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="navbar-logo-left-container shadow-three w-nav">
+    <div class="container">
+      <div class="navbar-wrapper">
+        <a href="#" class="navbar-brand w-nav-brand"><img src="images/USAFA-IOT-logos_transparent.png" loading="lazy" srcset="images/USAFA-IOT-logos_transparent.png 500w, images/USAFA-IOT-logos_transparent.png 957w" sizes="100vw" alt=""></a>
+        <nav role="navigation" class="nav-menu-wrapper w-nav-menu">
+          <ul role="list" class="nav-menu-two w-list-unstyled">
+            <li>
+              <a href="#" class="nav-link">Home</a>
+            </li>
+            <li>
+              <a href="#" class="nav-link">About</a>
+            </li>
+            <li>
+              <a href="#" class="nav-link">Contact</a>
+            </li>
+            <li>
+              <div data-hover="true" data-delay="0" class="nav-dropdown w-dropdown">
+                <div class="nav-dropdown-toggle w-dropdown-toggle">
+                  <div class="nav-dropdown-icon w-icon-dropdown-toggle"></div>
+                  <div class="text-block">Services</div>
+                </div>
+                <nav class="nav-dropdown-list shadow-three mobile-shadow-hide w-dropdown-list">
+                  <a href="#" class="nav-dropdown-link w-dropdown-link">Office Signs</a>
+                  <a href="#" class="nav-dropdown-link w-dropdown-link">Weather Station</a>
+                  <a href="#" class="nav-dropdown-link w-dropdown-link">Parking Lot</a>
+                  <div class="w-container"></div>
+                </nav>
+              </div>
+            </li>
+            <li>
+              <div class="nav-divider"></div>
+            </li>
+            <li class="mobile-margin-top-10">
+              <a href="#" class="button-primary w-button">Login</a>
+            </li>
+          </ul>
+        </nav>
+        <div class="menu-button w-nav-button">
+          <div class="w-icon-nav-menu"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-form">
+    <form id="wf-form-Login-Form" name="wf-form-Login-Form" data-name="Login Form" method="post" class="form form-2 form-3"><label for="name" class="field-label-2">Username</label><input type="text" class="text-field w-input" maxlength="256" name="name" data-name="Name" placeholder="Enter your username" id="name"><label for="email" class="field-label">Password</label><input type="password" class="text-field-2 w-input" maxlength="256" name="email" data-name="Email" placeholder="Enter your password" id="email" required=""><input type="submit" value="Login" data-wait="Please wait..." class="submit-button w-button">
+      <a href="#" class="button w-button">Register</a>
+    </form>
+    <div class="w-form-done">
+      <div>Thank you! Your submission has been received!</div>
+    </div>
+    <div class="w-form-fail">
+      <div>Oops! Something went wrong while submitting the form.</div>
+    </div>
+  </div>
+  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=632a181108141a036b8932b7" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="js/webflow.js" type="text/javascript"></script>
+  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+</body>
+</html>
