27a4d021e126f514643d76cf878e453225af2f14
funnygooberman <coffey.taylor70@gmail.com>
2022-09-20 18:38:43 -0600

    Update login.php

--- login.php
+++ login.php
@@ -165,7 +165,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
       <a href="register.php" class="button w-button">Register</a>
     </form>
   </div>
-  <!--<script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=632a181108141a036b8932b7" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>-->
+  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=632a181108141a036b8932b7" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <script src="js/webflow.js" type="text/javascript"></script>
   <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
 </body>
