6c446b599caa1a3cc7fb9acdb76e35844fea74f2
funnygooberman <coffey.taylor70@gmail.com>
2022-09-22 15:40:20 -0600

    Update test.php

--- test.php
+++ test.php
@@ -6,26 +6,18 @@ session_start();
 // Check if the user is logged in, if not then redirect him to login page
 require_once "database.php";
 //$php_id = $_SESSION["admin_id"];
-$check_query = "SELECT * FROM pi_ping_data";
+$check_query = "SELECT timestamp FROM pi_ping_data";
 $result = db_query($check_query);
 $num_online = 0;
 $num_offline = 0;
-echo "Hello \n";
-echo "num_online: ";
-echo $num_online;
-echo "\n num_offline: ";
-echo $num_offline;
+
 
 
 while($row = $result->fetch_assoc()){
-    $hostname = $row['hostname'];
-    $network = $row['network'];
-    $ip_addr = $row['ip_addr'];
-    $timestamp = $row['timestamp'];
-    $payload = $row['payload'];
+    $timestamps[] = $row['timestamp'];
   }
 
-  foreach($timestamp as $item) {
+  foreach($timestamps as $item) {
     echo $item;
     $date = date('Y-m-d H:i:s');
     $difference_in_seconds = strtotime($date) - strtotime($item);
@@ -36,4 +28,10 @@ while($row = $result->fetch_assoc()){
       $num_online = $num_online + 1;
     }
   }
+  echo "Hello \n";
+echo "num_online: ";
+echo $num_online;
+echo "\n num_offline: ";
+echo $num_offline;
+
 ?>
