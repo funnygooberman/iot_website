96d6eb4dcc88aa52c0a148ab142f2b5943308c66
funnygooberman <coffey.taylor70@gmail.com>
2022-09-22 12:23:27 -0600

    Update api_v1.php

--- api_v1.php
+++ api_v1.php
@@ -4,13 +4,16 @@
 	
 	// Initialize the session
     session_start();
-
-    $api_key = $_POST['api_key'];
+    print_r($_POST);
+    $api_key = $hostname = isset($_POST['api_key']) ? $_POST["api_key"]: $hostname = "";
 
     if($api_key == "12345") {
-        $api_function = $_POST['api_function'];
+        $api_function = isset($_POST['api_function']) ? $_POST["api_function"]: $hostname = "";
         if ($api_function == "ping") {
             ping_func();
+        }
+        else {
+            echo "Incorrect function";
         }
     }
 
@@ -22,7 +25,6 @@
     //echo "API KEY = " . $apiKey;
 
     function ping_func() {
-        print_r($_POST);
         $hostname = isset($_POST['hostname']) ? $_POST["hostname"]: $hostname = "";
         $network = isset($_POST['network']) ? $_POST["network"]: $network = "";
         $ip_addr = isset($_POST['ip']) ? $_POST["ip"]: $ip_addr = "";
