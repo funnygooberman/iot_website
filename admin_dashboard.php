9343fdfb4aebf84f90dec8e0350e4c09b0730b0c
funnygooberman <coffey.taylor70@gmail.com>
2022-09-22 15:33:33 -0600

    Update admin_dashboard.php

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -100,6 +100,7 @@ while($row = $result->fetch_assoc()){
   <div class="w-header">
     <p> Admin Page </p>
     <p> Number of PI's Online: <?php echo $num_online?></p>
+    <p> Number of PI's Offline: <?php echo $num_offline?></p>
 
   </div>
   <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=632a181108141a036b8932b7" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
