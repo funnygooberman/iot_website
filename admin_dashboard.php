8e7e772a7cad9e109b7611629f80e1c7e19e2f28
funnygooberman <coffey.taylor70@gmail.com>
2022-09-22 16:09:38 -0600

    Update admin_dashboard.php

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -39,7 +39,7 @@ while($row = $result->fetch_assoc()){
 <html data-wf-page="632a394cfcc00bc1310719e9" data-wf-site="632a181108141a036b8932b7">
 <head>
   <meta charset="utf-8">
-  <title>Dashboard</title>
+  <title>Admin Dashboard</title>
   <meta content="Dashboard" property="og:title">
   <meta content="Dashboard" property="twitter:title">
   <meta content="width=device-width, initial-scale=1" name="viewport">
@@ -95,12 +95,18 @@ while($row = $result->fetch_assoc()){
       </div>
     </div>
   </div>
-  <div class="w-header">
-    <p> Admin Page </p>
-    <p> Number of PI's Online: <?php echo $num_online?></p>
-    <p> Number of PI's Offline: <?php echo $num_offline?></p>
-
-  </div>
+  <section class="hero-heading-center wf-section">
+    <div class="container">
+      <h1 class="centered-heading margin-bottom-32px">PI Dashboard</h1>
+      <div class="hero-wrapper">
+        <div class="hero-split">
+          <p class="margin-bottom-24px">Number of PI's Online: <?php echo $num_online?></p>
+          <p class="margin-bottom-24px">Number of PI's Offline: <?php echo $num_offline?></p>
+        </div>
+        <a href="#" class="button-primary-2 w-button">View Full Ping Data</a>
+      </div>
+    </div>
+  </section>
   <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=632a181108141a036b8932b7" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <script src="js/webflow.js" type="text/javascript"></script>
   <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
